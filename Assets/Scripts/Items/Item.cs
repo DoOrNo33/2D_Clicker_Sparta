462747937736cd3f8e2a7f01369ac7d00251c74d
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-06-17 10:09:05 +0900

    [Feature] 아이템 구현

--- Assets/Scripts/Items/Item.cs
+++ Assets/Scripts/Items/Item.cs
@@ -1,20 +1,45 @@
+using TMPro;
 using UnityEngine;
+
+public enum TextType
+{
+    Level,
+    Cost,
+    Stats
+}
 
 public class Item : MonoBehaviour, Purchase
 {
-    public ClickSO ClickSO;
+    public ClickSO clickSO;
+    [SerializeField] protected TextMeshProUGUI[] itemStatsText;
 
-    [SerializeField] protected int Level;
-    [SerializeField] protected int cost;
-    [SerializeField] protected int ItemValue;
 
-    protected virtual void Awake()
+    protected virtual void Start()
     {
-        ClickSO = GameManager.Instance.ClickSO;
+        clickSO = GameManager.Instance.ClickSO;
+        itemStatsText = GetComponentsInChildren<TextMeshProUGUI>();
     }
 
     public virtual void PurchaseItem()
     {
         
+    }
+
+    protected virtual int IncreaseCost(int baseCost, int level)
+    {
+        //피보나치 수열 계산 코드
+
+        int a = baseCost;
+        int b = baseCost;
+        int c = 0;
+
+        for (int i = 0; i < level; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return c;
     }
 }
--- Assets/Scripts/Items/ItemNumOne.cs
+++ Assets/Scripts/Items/ItemNumOne.cs
@@ -1,20 +1,38 @@
-﻿using System.Diagnostics;
+﻿using UnityEngine;
 
 public class ItemNumOne : Item
 {
-    protected override void Awake()
+
+    [SerializeField][Range(0, 10)] private int maxLevel;
+    [SerializeField][Range(0, 10)] protected int level;
+
+    [SerializeField] private int baseCost;
+    [SerializeField] protected int cost;
+    [SerializeField] protected int itemValue;
+
+    protected override void Start()
     {
-        base.Awake();
+        base.Start();
+
+        itemStatsText[(int)TextType.Level].text = $"레벨 : {level} / {maxLevel}";
+        itemStatsText[(int)TextType.Cost].text = $"구매 비용 : {cost}";
     }
 
     public override void PurchaseItem()
     {
         bool isPurchased = (cost <= GameManager.Instance.EnergyController.energyAmount);
-        if (isPurchased)
+        bool isMaxLevel = (level < maxLevel);
+        if (isPurchased && isMaxLevel)
         {
-            GameManager.Instance.EnergyController.ChangeEnergyAmount(-cost);
-            ClickSO.ClickValue += ItemValue;
-            cost = (int)(cost * 1.5f);
+            GameManager.Instance.CallChangeEnergyEvent(-cost);
+
+            clickSO.ClickValue += itemValue;
+
+            level++;
+            cost = IncreaseCost(baseCost, level);
+
+            itemStatsText[(int)TextType.Level].text = $"레벨 : {level} / {maxLevel}";
+            itemStatsText[(int)TextType.Cost].text = $"구매 비용 : {cost}";
         }
     }
 }
