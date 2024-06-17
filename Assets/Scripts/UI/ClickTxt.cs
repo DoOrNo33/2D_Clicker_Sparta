1a99600ab71242ac023cd9a2afb133a4d8e426d6
DoOrNo33 <167051416+DoOrNo33@users.noreply.github.com>
2024-06-17 16:30:33 +0900

    [Feature] 클릭 애니메이션 오브젝트풀 구현

--- Assets/Scripts/UI/ClickTxt.cs
+++ Assets/Scripts/UI/ClickTxt.cs
@@ -4,27 +4,35 @@ using UnityEngine.UI;
 
 public class ClickTxt : MonoBehaviour
 {
-    public TextMeshProUGUI txt;
+    //public TextMeshProUGUI txt;
 
-    private Animator anim;
-    private int HashClick;
+    //private Animator anim;
+    //private int HashClick;
 
     private void Start()
     {
-        txt = GetComponent<TextMeshProUGUI>();
-        GameManager.Instance.ClickEvent += SetText;
-        GameManager.Instance.ClickEvent += PlayAnim;
-        anim = GetComponent<Animator>();
-        HashClick = Animator.StringToHash("Click");
+        //txt = GetComponent<TextMeshProUGUI>();
+        //GameManager.Instance.ClickEvent += SetText;
+        //GameManager.Instance.ClickEvent += PlayAnim;
+        //anim = GetComponent<Animator>();
+        //HashClick = Animator.StringToHash("Click");
     }
 
-    public void SetText(int Value)
+    //public void SetText(int Value)
+    //{
+    //    txt.text = Value.ToString();
+    //}
+
+    //public void PlayAnim(int Value)
+    //{
+    //    anim.SetTrigger(HashClick);
+    //}
+
+    public void TxtDisable()
     {
-        txt.text = Value.ToString();
+        gameObject.SetActive(false);
     }
 
-    public void PlayAnim(int Value)
-    {
-        anim.SetTrigger(HashClick);
-    }
+    //애니메이션 이벤트로 함수 발동하려면
+
 }
